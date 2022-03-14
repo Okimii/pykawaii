50e61938a7fef8af86e82a64a70a12b6490e6ad9
Okimii <92546867+Okimii@users.noreply.github.com>
2022-03-13 21:57:59 -0300

    Create nsfw.py

---
+++ waifupy/nsfw.py
@@ -0,0 +1,23 @@
+from httpclient import HTTPClient
+
+
+class NotSafeForWork:
+
+    _http = HTTPClient()
+    _type = "nsfw"
+    _url = "url"
+
+    async def waifu(self) -> str:
+        return (await self._http._request(self._type, "waifu"))[self._url]
+
+    async def neko(self) -> str:
+        return (await self._http._request(self._type, "neko"))[self._url]
+
+    async def trap(self) -> str:
+        return (await self._http._request(self._type, "trap"))[self._url]
+
+    async def blowjob(self) -> str:
+        return (await self._http._request(self._type, "blowjob"))[self._url]
+
+
+Nsfw = NotSafeForWork()
